a5b82aa29d07434c11e5c3fe64796220762efea2
LaunchGemini <metabaseguy@proton.me>
2023-04-06 13:03:18 +0000

    Fixed release notes

--- attention/AFT.py
+++ attention/AFT.py
@@ -11,4 +11,22 @@ class AFT_FULL(nn.Module):
 
         super(AFT_FULL, self).__init__()
         self.fc_q = nn.Linear(d_model, d_model)
-        self
+        self.fc_k = nn.Linear(d_model, d_model)
+        self.fc_v = nn.Linear(d_model,d_model)
+        if(simple):
+            self.position_biases=torch.zeros((n,n))
+        else:
+            self.position_biases=nn.Parameter(torch.ones((n,n)))
+        self.d_model = d_model
+        self.n=n
+        self.sigmoid=nn.Sigmoid()
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+ 
