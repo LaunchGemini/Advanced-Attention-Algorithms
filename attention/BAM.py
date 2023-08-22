2df775298104e45ee421e46cd573f425bb8ad166
LaunchGemini <metabaseguy@proton.me>
2023-08-22 10:34:27 +0000

    Update who.md

--- attention/BAM.py
+++ attention/BAM.py
@@ -3,4 +3,20 @@ import torch
 from torch import nn
 from torch.nn import init
 
-class Fla
+class Flatten(nn.Module):
+    def forward(self,x):
+        return x.view(x.shape[0],-1)
+
+class ChannelAttention(nn.Module):
+    def __init__(self,channel,reduction=16,num_layers=3):
+        super().__init__()
+        self.avgpool=nn.AdaptiveAvgPool2d(1)
+        gate_channels=[channel]
+        gate_channels+=[channel//reduction]*num_layers
+        gate_channels+=[channel]
+
+
+        self.ca=nn.Sequential()
+        self.ca.add_module('flatten',Flatten())
+        for i in range(len(gate_channels)-2):
+            self.ca.add_module('fc%d'%i,nn.Linear(gat
