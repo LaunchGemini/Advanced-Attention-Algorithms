0084d911e53ef89cc0b239754c200936700a41c4
LaunchGemini <metabaseguy@proton.me>
2023-08-22 16:23:33 +0000

    Bump up version number

--- attention/BAM.py
+++ attention/BAM.py
@@ -19,4 +19,19 @@ class ChannelAttention(nn.Module):
         self.ca=nn.Sequential()
         self.ca.add_module('flatten',Flatten())
         for i in range(len(gate_channels)-2):
-            self.ca.add_module('fc%d'%i,nn.Linear(gat
+            self.ca.add_module('fc%d'%i,nn.Linear(gate_channels[i],gate_channels[i+1]))
+            self.ca.add_module('bn%d'%i,nn.BatchNorm1d(gate_channels[i+1]))
+            self.ca.add_module('relu%d'%i,nn.ReLU())
+        self.ca.add_module('last_fc',nn.Linear(gate_channels[-2],gate_channels[-1]))
+        
+
+    def forward(self, x) :
+        res=self.avgpool(x)
+        res=self.ca(res)
+        res=res.unsqueeze(-1).unsqueeze(-1).expand_as(x)
+        return res
+
+class SpatialAttention(nn.Module):
+    def __init__(self,channel,reduction=16,num_layers=3,dia_val=2):
+        super().__init__()
+     
