0a26ddb7d6bf34a69097fe38324ddab1f39f736b
LaunchGemini <metabaseguy@proton.me>
2023-08-29 18:07:14 +0000

    hemma

--- attention/CoTAttention.py
+++ attention/CoTAttention.py
@@ -2,3 +2,23 @@ import numpy as np
 import torch
 from torch import flatten, nn
 from torch.nn import init
+from torch.nn.modules.activation import ReLU
+from torch.nn.modules.batchnorm import BatchNorm2d
+from torch.nn import functional as F
+
+
+
+class CoTAttention(nn.Module):
+
+    def __init__(self, dim=512,kernel_size=3):
+        super().__init__()
+        self.dim=dim
+        self.kernel_size=kernel_size
+
+        self.key_embed=nn.Sequential(
+            nn.Conv2d(dim,dim,kernel_size=kernel_size,padding=kernel_size//2,groups=4,bias=False),
+            nn.BatchNorm2d(dim),
+            nn.ReLU()
+        )
+        self.value_embed=nn.Sequential(
+            nn.Conv2d(dim,di
