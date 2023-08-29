0793ef24992448226807b0b4a2e5ff572a216ccf
LaunchGemini <metabaseguy@proton.me>
2023-08-29 08:17:32 +0000

    Update Travis CI

--- attention/BAM.py
+++ attention/BAM.py
@@ -61,4 +61,21 @@ class BAMBlock(nn.Module):
         self.sigmoid=nn.Sigmoid()
 
 
-    def
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        sa_out=self.sa(x)
+       
