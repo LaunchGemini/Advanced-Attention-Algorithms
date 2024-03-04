b129bb478d99c43bbbf4b112fabd96d4fa84b20c
LaunchGemini <metabaseguy@proton.me>
2024-03-04 20:15:24 +0000

    Fix text.

--- attention/PSA.py
+++ attention/PSA.py
@@ -32,4 +32,20 @@ class PSA(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not N
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Step1:SPC module
+        SPC_out=x.view(b,self.S,c//self.S,h,w) #bs,s,ci,h,w
+        for idx,conv in enumerate(self.convs):
+      
