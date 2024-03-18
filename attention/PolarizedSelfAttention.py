a43eeec63a5fc44b7f30928e289f18a89bc8bebb
LaunchGemini <metabaseguy@proton.me>
2024-03-18 14:27:18 +0000

    Fixing whitespace problems

--- attention/PolarizedSelfAttention.py
+++ attention/PolarizedSelfAttention.py
@@ -15,3 +15,18 @@ class ParallelPolarizedSelfAttention(nn.Module):
         self.softmax_spatial=nn.Softmax(-1)
         self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
         self.ln=nn.LayerNorm(channel)
+        self.sigmoid=nn.Sigmoid()
+        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.agp=nn.AdaptiveAvgPool2d((1,1))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Channel-only Self-Attention
+        channel_wv=self.ch_wv(x) #bs,c//2,h,w
+        channel_wq=self.ch_wq(x) #bs,1,h,w
+        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
+        channel_wq=self.softmax_channel(channel_wq)
+        channel_wz=torch.matmu
