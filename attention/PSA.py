020a0f81936ba7bdc1d47b3fe2b7e626deadd066
LaunchGemini <metabaseguy@proton.me>
2024-03-18 13:15:48 +0000

    update sbt plugins

--- attention/PSA.py
+++ attention/PSA.py
@@ -69,4 +69,4 @@ if __name__ == '__main__':
     input=torch.randn(50,512,7,7)
     psa = PSA(channel=512,reduction=8)
     output=psa(input)
-  
+    print(output.shape)
---
+++ attention/PolarizedSelfAttention.py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import init
+
+
+
+class ParallelPolarizedSelfAttention(nn.Module):
+
+    def __init__(self, channel=512):
+        super().__init__()
+        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
+        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
+        self.ln=nn.LayerNorm(channel)
