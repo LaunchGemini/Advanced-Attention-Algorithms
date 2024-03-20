7c45c89dfe6668438ab09a9e9f4028ff742ca97a
LaunchGemini <metabaseguy@proton.me>
2024-03-20 15:47:50 +0000

    [MAPTILES] debugging

--- attention/PolarizedSelfAttention.py
+++ attention/PolarizedSelfAttention.py
@@ -70,4 +70,11 @@ class SequentialPolarizedSelfAttention(nn.Module):
         b, c, h, w = x.size()
 
         #Channel-only Self-Attention
-     
+        channel_wv=self.ch_wv(x) #bs,c//2,h,w
+        channel_wq=self.ch_wq(x) #bs,1,h,w
+        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
+        channel_wq=self.softmax_channel(channel_wq)
+        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
+        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
+        channel_out=channel_wei
