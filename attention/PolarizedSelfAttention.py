1a23593a0547658496d5fb6e32c023f616b63980
LaunchGemini <metabaseguy@proton.me>
2024-03-20 09:49:41 +0000

    Portfolio

--- attention/PolarizedSelfAttention.py
+++ attention/PolarizedSelfAttention.py
@@ -55,4 +55,19 @@ class SequentialPolarizedSelfAttention(nn.Module):
 
     def __init__(self, channel=512):
         super().__init__()
-        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_siz
+        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
+        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
+        self.ln=nn.LayerNorm(channel)
+        self.sigmoid=nn.Sigmoid()
+        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.agp=nn.AdaptiveAvgPool2d((1,1))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Channel-only Self-Attention
+     
