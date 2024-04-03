decd324a7f2b65b77d343f897f22234b9b9c586b
LaunchGemini <metabaseguy@proton.me>
2024-04-03 12:45:46 +0000

    Update SUMMARY.adoc

--- attention/SKAttention.py
+++ attention/SKAttention.py
@@ -1,4 +1,20 @@
 import numpy as np
 import torch
 from torch import nn
-from torch.nn impor
+from torch.nn import init
+from collections import OrderedDict
+
+
+
+class SKAttention(nn.Module):
+
+    def __init__(self, channel=512,kernels=[1,3,5,7],reduction=16,group=1,L=32):
+        super().__init__()
+        self.d=max(L,channel//reduction)
+        self.convs=nn.ModuleList([])
+        for k in kernels:
+            self.convs.append(
+                nn.Sequential(OrderedDict([
+                    ('conv',nn.Conv2d(channel,channel,kernel_size=k,padding=k//2,groups=group)),
+                    ('bn',nn.BatchNorm2d(channel)),
+                  
