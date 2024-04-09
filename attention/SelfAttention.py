1e6a653cc35258d414d4ee18174cd7b499144a2a
LaunchGemini <metabaseguy@proton.me>
2024-04-09 08:33:42 +0000

    2.7.5

--- attention/SelfAttention.py
+++ attention/SelfAttention.py
@@ -18,4 +18,23 @@ class ScaledDotProductAttention(nn.Module):
         :param h: Number of heads
         '''
         super(ScaledDotProductAttention, self).__init__()
-        self.fc_
+        self.fc_q = nn.Linear(d_model, h * d_k)
+        self.fc_k = nn.Linear(d_model, h * d_k)
+        self.fc_v = nn.Linear(d_model, h * d_v)
+        self.fc_o = nn.Linear(h * d_v, d_model)
+        self.dropout=nn.Dropout(dropout)
+
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.h = h
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+       
