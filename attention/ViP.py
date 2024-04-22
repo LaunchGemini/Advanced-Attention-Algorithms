c5e15f7994ca7917ac6300e02d349590eaa285e1
LaunchGemini <metabaseguy@proton.me>
2024-04-22 13:26:58 +0000

    benis graph

--- attention/ViP.py
+++ attention/ViP.py
@@ -8,4 +8,23 @@ class MLP(nn.Module):
         self.fc1=nn.Linear(in_features,hidden_features)
         self.act=act_layer()
         self.fc2=nn.Linear(hidden_features,out_features)
-        self.drop=n
+        self.drop=nn.Dropout(drop)
+
+    def forward(self, x) :
+        return self.drop(self.fc2(self.drop(self.act(self.fc1(x)))))
+
+class WeightedPermuteMLP(nn.Module):
+    def __init__(self,dim,seg_dim=8, qkv_bias=False, proj_drop=0.):
+        super().__init__()
+        self.seg_dim=seg_dim
+
+        self.mlp_c=nn.Linear(dim,dim,bias=qkv_bias)
+        self.mlp_h=nn.Linear(dim,dim,bias=qkv_bias)
+        self.mlp_w=nn.Linear(dim,dim,bias=qkv_bias)
+
+        self.reweighting=MLP(dim,dim//4,dim*3)
+
+        self.proj=nn.Linear(dim,dim)
+        self.proj_drop=nn.Dropout(proj_drop)
+    
+ 
